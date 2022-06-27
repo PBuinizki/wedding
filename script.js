124acaffe2037139c20dd9e4e4096f493f069db0
PBuinizki <61580868+PBuinizki@users.noreply.github.com>
2022-06-27 20:42:07 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 "use strict"
+
+const swiper = new Swiper('.swiper', {
+    speed: 400,
+    spaceBetween: 100,
+    slidesPerView: 2,
+    centeredSlides: true,
+    loop: true,
+    pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+    autoplay: {
+            delay: 2500,
+            disableOnInteraction: false,
+          },
+  });
